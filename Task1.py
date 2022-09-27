29e2c6d5ed35c8be5802be64b5956943c666a306
Anastasia <nastasy.kozh@gmail.com>
2022-09-27 05:53:34 +0300

    Modify Task1

--- Task1.py
+++ Task1.py
@@ -1,5 +1,5 @@
 # txt = 'Напишите абв пабврограмму программу, удаляющую из этого незабвенного текста все абвслова слова, содерабващие содержащие последовательность букв а, б и в "абв"'
-txt = open('Task1Text.txt')
+txt = open('Task1Text.txt', 'r')
 
 def remove(txt):
     txt = list(filter(lambda x: 'абв' not in x, txt.split()))
